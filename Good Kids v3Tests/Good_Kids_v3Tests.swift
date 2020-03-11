beb818c4adfd7596d631ee6960b45b1224dbdc13
Aleksandr Ozhogin <aaozhogin@gmail.com>
2020-03-11 20:02:25 +1100

    Initial Commit

---
+++ Good Kids v3Tests/Good_Kids_v3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Good_Kids_v3Tests.swift
+//  Good Kids v3Tests
+//
+//  Created by Aleksandr Ozhogin on 11/3/20.
+//  Copyright © 2020 Aleksandr Ozhogin. All rights reserved.
+//
+
+import XCTest
+@testable import Good_Kids_v3
+
+class Good_Kids_v3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
